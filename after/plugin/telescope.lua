3fa5aa0f0ea96a14ae4ebbd7396f6969c0e7e700
Chuck J Hardy <hardy@hey.com>
2023-01-07 14:57:30 +0000

    Telescope File Browser

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,8 @@
+require("telescope").load_extension "file_browser"
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.api.nvim_set_keymap("n", "<leader><leader>", ":Telescope file_browser<CR>", { noremap = true })
--- lua/chuckjhardy/packer.lua
+++ lua/chuckjhardy/packer.lua
@@ -48,4 +48,6 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    use { "nvim-telescope/telescope-file-browser.nvim" }
 end)
